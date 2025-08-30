cb45425d117de0a54c794c6f5d3a96809a164074
Evilly Costa <evillynascicosta@gmail.com>
2025-08-29 21:36:32 -0300

    learn(ap04-react): encerra a apostila

--- ap04-react-components-props/src/App.jsx
+++ ap04-react-components-props/src/App.jsx
@@ -1,40 +1,76 @@
 import Pedido from "./Pedido"
+import Cartao from "./Cartao"
+import Hippo from "./Hippo"
+import Feedback from "./Feedback"
+
+//
+//criar um componente Hippo que produz um hipopotamo
 const App = () => {
+  const textoOK = "Já chegou!"
+  const textoNOK = "Ainda não chegou"
+  const funcaoOK = () => alert("Obrigado pelo feedback")
+  const funcaoNOK = () => alert("Verificaremos")
+  const componenteFeedback = (
+    <Feedback
+      textoOK={textoOK}
+      textoNOK={textoNOK}
+      funcaoOK={funcaoOK}
+      funcaoNOK={funcaoNOK}/>
+  )
+
+  const pedidos = [
+    {
+      data: '22/08/2025',
+      icone: 'fa-solid fa-heart',
+      titulo: 'Coração',
+      descricao: 'Coração bom'
+    },
+    {
+      data: '21/08/2025',
+      icone: 'fa-solid fa-book',
+      titulo: 'Concrete Maths',
+      descricao: 'Autor Donald'
+    },
+    {
+      data: '22/08/2025',
+      icone: 'fa-solid fa-cat',
+      titulo: 'Gato',
+      descricao: 'Autor Donald'
+    },
+    {
+      data: '22/08/2025',
+      icone: 'fa-solid fa-hippo',
+      titulo: 'Hipopótamo',
+      descricao: 'Autor Donald'
+    },
+  ]
   return(
     <div className="container">
       <div className="row">
-        <div className="col-12 col-lg-6 col-xxl-3">
-          <Pedido 
-          data="22/08/2025"
-          icone="fa-solid fa-heart"
-          titulo="Coração"
-          descricao="Coração bom"
-          />
+        <div className="col-12">
+          <Hippo size='fa-4x'/>
+          <span className="mx-1"></span>
+          <Hippo
+            size= 'fa-3x'
+            rotate='fa-flip-horizontal'/>
         </div>
-        <div className="col-12 col-lg-6 col-xxl-3">
-          <Pedido 
-          data="21/08/2025"
-          icone="fa-solid fa-book"
-          titulo="Concrete Maths"
-          descricao="Autor Donald"
-          />
-        </div>
-        <div className="col-12 col-lg-6 col-xxl-3">
-          <Pedido
-          data="21/08/2025"
-          icone="fa-solid fa-cat"
-          titulo="Gato"
-          descricao="Autor Donald"
-          />
-        </div>
-        <div className="col-12 col-lg-6 col-xxl-3">
-          <Pedido 
-          data="21/08/2025"
-          icone="fa-solid fa-hippo"
-          titulo="Hipopótamo"
-          descricao="Autor Donald"
-          />
-        </div>
+      </div>
+      <div className="row">
+        {
+          pedidos.map(pedido => (
+            <div className="col-12 col-lg-6 col-xxl-3">
+              <Cartao
+                className='mb-2'
+                cabecalho={pedido.data}>
+                  <Pedido 
+                    icone={pedido.icone}
+                    titulo={pedido.titulo}
+                    descricao={pedido.descricao}/>
+                    {componenteFeedback}
+              </Cartao>
+            </div>
+          ))
+        }
       </div>
     </div>
   )
---
+++ ap04-react-components-props/src/Hippo.jsx
@@ -0,0 +1,8 @@
+const Hippo = (props) => {
+    return (
+        <i className = {`fa-solid fa-hippo ${props.size} 
+            ${props.rotate}`}/>
+    )
+}
+
+export default Hippo
