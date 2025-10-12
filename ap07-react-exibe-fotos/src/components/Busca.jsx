d943c62d2af08c0d2b495a5dcdc88de9aa83495a
Evilly Costa <evillynascicosta@gmail.com>
2025-10-12 14:54:41 -0300

    learn(ap07-react): encerra a secao 2.5
    - retomar fazendo a funcao de busca

---
+++ ap07-react-exibe-fotos/src/components/Busca.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { Button } from 'primereact/button'
+import  { IconField } from 'primereact/IconField'
+import  { InputIcon } from 'primereact/InputIcon'
+import  { InputText } from 'primereact/InputText'
+export default class Busca extends Component {
+
+  state = {
+    termoDeBusca: ''
+  }
+
+  onTermoAlterado = (evento) => {
+    // console.log(evento.target.value)
+    this.setState({
+      termoDeBusca: evento.target.value
+    })
+  }
+
+  onFormSubmit = (evento) => {
+    evento.preventDefault()  
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <div className='flex flex-column'>
+          <IconField 
+            className='w-full'
+            iconPosition='left'>
+            <InputIcon className='pi pi-search'/>
+            <InputText
+              value={this.state.termoDeBusca}
+              onChange={this.onTermoAlterado}
+              className='w-full' 
+              placeholder={this.props.dica}
+            />
+          </IconField>
+          <Button 
+            className='mt-3'
+            label='OK'/>
+        </div>
+      </form>
+    )
+  }
+}
+
+Busca.defaultProps = {
+  dica: 'Buscar...'
+}
